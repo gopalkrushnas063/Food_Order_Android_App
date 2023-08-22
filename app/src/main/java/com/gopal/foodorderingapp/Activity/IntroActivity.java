6dde61eeb5cb59b4097cde438fa6037ed668867d
gopalkrushnas063 <gopalkrushnas063@gmail.com>
2023-08-22 10:13:52 +0530

    Category Section Recycler View , adapter and static content added

--- app/src/main/java/com/gopal/foodorderingapp/Activity/IntroActivity.java
+++ app/src/main/java/com/gopal/foodorderingapp/Activity/IntroActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import com.gopal.foodorderingapp.MainActivity;
 import com.gopal.foodorderingapp.R;
 
 public class IntroActivity extends AppCompatActivity {
